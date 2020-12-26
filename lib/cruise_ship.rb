0f996883f4f10637a01b631cdd99e576d17f5017
Harrf Akbar <harrf_akbar@yahoo.com>
2020-12-26 20:28:00 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -11,6 +11,7 @@ def select_winner(passengers)
   winner = ""
   passengers.each do | suite, name |
     if suite == :suite_a && name.start_with?("A")
-      winner = name 
-    end 
+      winner = name
+    end
+  end
 end
